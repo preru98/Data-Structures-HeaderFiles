a8f479b47eb14792043b24c99fb6c8ca0a8119c9
Prerna <prerna1998.ps@gmail.com>
2019-10-11 23:37:18 +0530

    Binary search tree

---
+++ Trees/driverClass.java
@@ -0,0 +1,26 @@
+import java .util.Scanner;
+public class driverClass{
+    public static void main(String args[]){
+        int val;
+        Scanner sc =new Scanner(System.in);
+        val=sc.nextInt();
+        // Node=new Node(val,null,null);
+
+    }
+}
+class createNode{
+    int data;
+    Node leftNode;
+    Node rightNode;
+    
+    Node(int data, Node leftNode, Node rightNode){
+        data=this.data;
+        leftNode=this.leftNode;
+        rightNode=this.rightNode;
+    }
+
+}
+
+class addNode{
+
+}
