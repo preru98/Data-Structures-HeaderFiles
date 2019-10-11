37fea80634a3568c5c6ac882127225a27bdfefbe
Prerna <prerna1998.ps@gmail.com>
2019-10-12 01:58:30 +0530

    binary search tree continued!

--- Trees/driverClass.java
+++ Trees/driverClass.java
@@ -4,11 +4,35 @@ public class driverClass{
         int val;
         Scanner sc =new Scanner(System.in);
         val=sc.nextInt();
-        // Node=new Node(val,null,null);
+        tree T=new tree(val);
+
 
     }
 }
-class createNode{
+class tree{
+    Node rootNode;
+    tree(int rootData){
+        rootNode=new Node(rootData,null,null); 
+    }
+    addNode(int data,Node n){
+        if(n.data>data){
+            if(n.leftNode!=null){
+                addNode(data,n.leftNode);
+            }
+            else
+            n.leftNode=new Node(data,null,null);
+        }
+        else{
+            if(n.rightNode!=null){
+                addNode(data,n.rightNode);
+            }
+            else{
+                n.rightNode=new Node(data,null,null);
+            }
+        }
+    }
+}
+class Node{
     int data;
     Node leftNode;
     Node rightNode;
@@ -18,7 +42,7 @@ class createNode{
         leftNode=this.leftNode;
         rightNode=this.rightNode;
     }
-
+   
 }
 
 class addNode{
