7eb9ad0e77a838d3b8048a5a1e961ed14047351c
Prerna <prerna1998.ps@gmail.com>
2019-10-15 02:03:25 +0530

    queue using arrays

--- Queues/queue.java
+++ Queues/queue.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class queue{
     int array[];
     int rear;
@@ -36,12 +37,39 @@ public class queue{
 } 
 class driverClass{
     public static void main(String args[]){
+        int val;
+        Scanner sc= new Scanner(System.in); 
+        int rep=1;
+        int ch;
         queue Q=new queue();
-        Q.add(10);
-        Q.add(20);
-        Q.add(30);
-        Q.display();
-        Q.remove();
-        Q.display();
+        while(rep==1){
+
+            System.out.println("Select Choice Accordingly :) ");
+            System.out.println("1.Add Element");
+            System.out.println("2.Delete Element");
+            System.out.println("3.Display Queue");
+
+            ch=sc.nextInt();
+            if(ch==1){
+                System.out.println("Enter value");
+                val=sc.nextInt();
+                Q.add(val);
+            }
+    
+            else if(ch==2){
+                Q.remove();
+            }
+
+            else if(ch==3){
+                Q.display();
+            }
+
+            else 
+                System.out.println("Wrong Choice.Aborting...");
+            
+        System.out.println("Do you want to continue? Press 1.");
+         rep=sc.nextInt();
+        } 
+     
     }
 }
