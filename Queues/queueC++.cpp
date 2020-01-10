5ee675a6a9c3167295c7d73448373ce22ae443d4
Prerna <prerna1998.ps@gmail.com>
2020-01-10 21:15:57 +0530

    Queues implementation in c++ via STL

---
+++ Queues/queueC++.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+void showQueue(queue <string>);
+int main(){
+    queue <string> Q;
+
+    //ENQUEUE 
+    
+    Q.push("Hi");
+    Q.push("I hope you having a good day.");
+    Q.push("Take care!");
+
+    //User defined func. to display the contents of Queue
+    cout<<"Queue after insertion\n";
+    showQueue(Q);
+ 
+    //Built in function
+    // cout<<Q.front();
+    // cout<<Q.back();
+
+    //Assignment to temporary variable
+    string s=Q.front();
+    
+    //DEQUEUE
+    Q.pop();
+    Q.pop();
+    cout<<"Queue after deletion\n";
+    showQueue(Q);
+
+    return 0;
+}
+
+void showQueue(queue <string> Q){
+    while(!Q.empty()){
+        cout<<Q.front()<<"  ";
+        Q.pop();  
+    }
+    cout<<"\n";
+}
