584009f11f9df50c3223b98852a32ffd3e4d20f1
Prerna <prerna1998.ps@gmail.com>
2020-02-25 23:32:33 +0530

    Using our own headerfile

---
+++ linkedLists/headerfile/usingHeaderFile.cpp
@@ -0,0 +1,9 @@
+#include "linkedlistVer3.h"
+using namespace std;
+
+int main(){
+    int array[]={23,77,21,90, 56, 345};
+    int size=sizeof(array)/sizeof(array[0]);
+    insert_at_end_array(array,size);
+    display(start);
+}
