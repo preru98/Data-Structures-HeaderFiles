d5ac00f61d42b6810b147008b11ca176dd7caf27
Prerna <prerna1998.ps@gmail.com>
2020-03-09 23:17:14 +0530

    Unstable version 4 Header Linked List

--- linkedLists/headerfile/usingHeaderFile.cpp
+++ linkedLists/headerfile/usingHeaderFile.cpp
@@ -1,9 +1,14 @@
-#include "linkedlistVer3.h"
+#include "linkedlistVer4.h"
 using namespace std;
 
 int main(){
-    int array[]={23,77,21,90, 56, 345};
+     int array[]={23,35,91};
     int size=sizeof(array)/sizeof(array[0]);
+
+    int array2[]={29,36,37,105,206,409};
+    int size2=sizeof(array)/sizeof(array[0]);
     insert_at_end_array(array,size);
-    display(start);
+    insert_at_end_array2(array2,size2);
+    Node* head= merge(start,start2);
+    display(head);
 }
