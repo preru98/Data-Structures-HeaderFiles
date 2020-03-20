7327619e9e7eff3e8ccd71c1d829f328f742da14
Prerna <prerna1998.ps@gmail.com>
2020-03-20 08:30:47 +0530

    Binary Tree traversal : Preorder

---
+++ Trees/treesImplementationC++/preorderTraversal.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *lChild;
+    Node* rChild;
+};
+Node* root =NULL; 
+
+Node* create_node(int n);
+void preorder(Node *);
+void build_tree();
+
+queue<Node*>Q;
+
+int main(){
+    int ele;
+    cout<<"Enter Root Element";
+    cin>>ele;
+    cout<<"Creating Root...";
+    root=create_node(ele);
+    Q.push(root);
+    //Root Ready...
+    
+    while(!Q.empty()){
+      build_tree();
+    }
+
+    preorder(root);
+    return 0;
+}
+
+void preorder(Node *np){
+    cout<<np->data<<"  ";
+    
+    if(np->lChild!=NULL){
+        preorder(np->lChild);
+    }
+    if(np->rChild!=NULL){
+        preorder(np->rChild);
+    }
+}
+
+void build_tree(){
+    Node *temp,*ptr; 
+    int lc,rc;
+
+    ptr=Q.front();
+    Q.pop();
+
+    cout<<"LChild of "<<ptr->data<<" : ";
+    cin>>lc;
+    if(lc!=-1){
+        temp=create_node(lc);
+        ptr->lChild=temp;
+        Q.push(temp);
+    }
+        
+    cout<<"RChild of "<<ptr->data<<" : ";
+    cin>>rc;
+    if(rc!=-1){
+        temp=create_node(rc);
+        ptr->rChild=temp;
+        Q.push(temp);
+    }
+}
+
+Node* create_node(int n){
+    Node* temp=new Node;
+    temp->data=n;
+    temp->lChild=temp->rChild=NULL;
+}
