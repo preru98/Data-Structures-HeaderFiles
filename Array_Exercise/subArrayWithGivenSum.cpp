9891db0997056c9c6cf63d93cc234739dcf9f4d3
Prerna <prerna1998.ps@gmail.com>
2020-06-15 14:46:07 +0530

    subarray with given sum [imporoved] | GFG | 1

--- Array_Exercise/subArrayWithGivenSum.cpp
+++ Array_Exercise/subArrayWithGivenSum.cpp
@@ -1,3 +1,13 @@
+/*__________________________________________________________________________________________________________________________________________________________________________________________________
+Given an unsorted array A of size N of non-negative integers, find a continuous sub-array which adds to a given number S.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow. Each test case consists of two lines. 
+The first line of each test case is N and S, where N is the size of array and S is the sum. The second line of each test case contains N space separated integers denoting the array elements.
+
+Output:
+For each testcase, in a new line, print the starting and ending positions(1 indexing) of first such occuring subarray from the left if sum equals to subarray, else print -1.
+__________________________________________________________________________________________________________________________________________________________________________________________________*/
 #include<iostream>
 
 using namespace std;
@@ -22,30 +32,22 @@ int main(){
 
 void subarray_sum(int sum,int size,int array[]){
 
-    int sum_inter=0,end=0,flag_match_not_found=1;
+    int sum_inter=array[0], end=0, start=0, flag_match_not_found=1;
 
-    for(int i=0;i<size;i++){
-        
-        if(array[i]==sum){
-            cout<<i+1<<" "<<i+1<<"\n";
+    while(end<size){
+        if(sum_inter == sum){
+            cout<<start+1<<" "<<end+1;           
             flag_match_not_found=0;
             break;
         }
-
-        sum_inter=array[i];
-        end=i;
-
-        while(sum_inter<sum && end<size){
+        else if(sum_inter < sum){
             end++;
             sum_inter+=array[end];
         }
-
-        if(sum_inter==sum){
-            cout<<i+1<<" | "<<end+1<<"\n";
-            flag_match_not_found=0;
-            break;
+        else{
+            sum_inter-=array[start];
+            start++;
         }
-
     }
 
     if(flag_match_not_found){
