847a0f292f3b745409707e207a99c5da6aab6296
Prerna <prerna1998.ps@gmail.com>
2020-09-21 16:14:56 +0530

    Count Nodes in a Linked List | Using Structures

---
+++ linkedLists/countNodes.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node *next;
+};
+
+Node* create_node(int);
+void insert_at_end(int);
+void display(Node*);
+int count(Node* start);
+
+
+Node *start=NULL, *rear=NULL;
+
+int main(){
+    int list[] = {4,7,2,9,1,0,2,6,1,9,10};
+    int size =sizeof(list)/sizeof(list[0]);
+
+    for(int i=0;i<size;i++){
+        insert_at_end(list[i]);
+    }
+    display(start);
+
+    cout<<"\nCount: "<<count(start);
+    return 0;
+}
+
+Node* create_node(int info){
+    Node *new_node = new Node;
+    new_node->data=info;
+    new_node->next=NULL;
+    return new_node;
+}
+
+void insert_at_end(int info){
+    Node *new_node = create_node(info);
+    
+    if(start==NULL && rear==NULL){
+        start = rear =new_node;
+        return;
+    }
+    rear->next = new_node;
+    rear=new_node;
+}
+
+void display(Node* start){
+    while(start){
+        cout<<start->data<<" | ";
+        start=start->next;
+    }
+}
+
+int count(Node* start){
+    int count=0;
+    while(start){
+        count++;
+        start=start->next;
+    }
+    return count;
+}
+
+
+
+
