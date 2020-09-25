3aa69f3e3cc31faa82540e42cc73d8379802363a
Prerna <prerna1998.ps@gmail.com>
2020-09-26 04:13:09 +0530

    Checking Anagrams | Sorting | O(n) Time Complexity

---
+++ Strings/checkForAnagrams.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+bool check_anagrams_distinct(string, string);
+
+int main(){
+    string first = "decimal";
+    string second = "medical";
+    cout<<endl;
+    cout<<check_anagrams_distinct(first, second);
+    return 0;
+}
+
+bool check_anagrams_distinct(string first, string second){
+    if(first.size()!=second.size()){
+        return false;
+    }
+    int* table =  new int[26];
+
+    for(int i=0;i<first.size();i++){
+        table[int(first[i])-97]++;
+        table[int(second[i])-97]++;
+    }
+    
+    for(int i=0;i<first.size();i++){
+        if(table[int(first[i])-97]!=2){
+            return false;
+        }
+    }
+    return true;
+}
+
